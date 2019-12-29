8eb0753614acfce28f3ad00b649be28570766844
igor <embiggenerd@gmail.com>
2019-12-28 22:33:07 -0500

    login working as expected and returning access token

--- src/UserResolver.ts
+++ src/UserResolver.ts
@@ -1,6 +1,13 @@
-import { Mutation, Resolver, Query, Arg } from 'type-graphql'
-import { hash } from 'bcryptjs'
+import { Mutation, Resolver, Query, Arg, ObjectType, Field } from 'type-graphql'
+import { hash, compare } from 'bcryptjs'
 import { User } from './entity/User'
+import { sign } from 'jsonwebtoken'
+
+@ObjectType()
+class LoginResponse {
+    @Field()
+    accessToken: string
+}
 
 @Resolver()
 export class UserResolver {
@@ -9,10 +16,35 @@ export class UserResolver {
         return 'hi'
     }
 
-    @Query(()=>[User])
+    @Query(() => [User])
     users() {
         return User.find()
     }
+
+
+    @Mutation(() => LoginResponse)
+    async login(
+        @Arg('email') email: string,
+        @Arg('password') password: string
+    ): Promise<LoginResponse> {
+        const user = await User.findOne({ where: { email } })
+
+        if (!user) {
+            throw new Error('User not found')
+        }
+
+        const valid = await compare(password, user.password)
+
+        if (!valid) {
+            throw new Error('Bad password')
+        }
+
+        // successful login
+        return {
+            accessToken: sign({ userID: user.id }, "secret", { expiresIn: "15m" })
+        }
+    }
+
 
     @Mutation(() => Boolean)
     async register(
@@ -22,12 +54,12 @@ export class UserResolver {
         const hashedPass = await hash(password, 12)
 
         try {
-            
-        await User.insert({
-            email,
-            password: hashedPass
-        })
-        } catch(e){
+
+            await User.insert({
+                email,
+                password: hashedPass
+            })
+        } catch (e) {
             console.log(e)
             return false
         }
