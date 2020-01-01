d58d26ed23da7fffa0be919271d09222e671e720
igor <embiggenerd@gmail.com>
2020-01-01 16:36:38 -0500

    only endpoint that receives refresh token is now /refresh_token to minimize mim attacks on refresh token

--- client/src/pages/Header.tsx
+++ client/src/pages/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { useMeQuery, useLogoutMutation } from '../generated/graphql';
 import { setAccessToken } from '../accessToken';
 
-interface Props {}
+interface Props { }
 
 export const Header: React.FC<Props> = (props) => {
     const { data, loading } = useMeQuery({ fetchPolicy: "network-only" })
@@ -24,11 +24,11 @@ export const Header: React.FC<Props> = (props) => {
             <div><Link to="/register">Register</Link></div>
             <div><Link to="/bye">Bye</Link></div>
             <div><Link to="/">Home</Link></div>
-            <button onClick={async () => {
+            {!loading && data && data.me  ? <button onClick={async () => {
                 await logout()
                 setAccessToken(null)
                 await client!.resetStore()
-            }}>Logout</button>
+            }}>Logout</button> : null}
             {body}
         </header>
     )
